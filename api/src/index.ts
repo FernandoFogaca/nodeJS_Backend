127ada90c36ebdf2c16ea1fbf94e71b338edc95f
Fernando Fogaca <fernandopfogaca@gmail.com>
2024-08-22 01:13:52 +0200

    feat aula 0 - express com rotas get

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,1 +1,47 @@
-console.log('Embreve uma api')
+import express, { Request, Response } from 'express';
+
+const app = express();
+app.use(express.static('public'))
+const porta = 3030;
+
+app.get('/', (req, res): void => {
+  res.send('API with NodeJS and TS!');
+});
+
+app.get('/Ping', (req, res): void => {
+  res.send('Pong <Ó');
+});
+
+
+app.get('/soma', (req: Request, res: Response): void => {
+
+    // http://localhost:3030/soma?num1=100&num2=120&num3=500
+    const {num1, num2, num3}= req.query as { num1: string; num2: string; num3: string };
+    const soma = parseInt(num1, 10) + parseInt(num2, 10) + parseInt(num3, 10);
+  
+    res.send(`${soma}`);
+  });
+
+  app.get('/departamentos/:id', (req, res): void => {
+
+    console.log(req.params)
+    res.send('Pong <Ó');
+  });
+
+  //http://localhost:3030/departamentos
+  //http://localhost:3030/departamentos/25
+
+
+  app.get('/clientes', (req:Request, res:Response): void => {
+
+    const html: string = '<link href="css/style.css" rel="stylesheet"><h1>Clientes</h1>';
+
+    res.send(html);
+  });
+
+
+
+app.listen(porta, () => {
+  console.log(`Server is running on port ${porta}`);
+});
+
